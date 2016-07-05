e47d778c33e42c2c1d861603cde6d5a20742e7b2
Paddy Reynolds <paddy@pc88.home>
2016-07-06 07:10:25 +1000

    Initial Commit

---
+++ Text Editor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Text Editor
+//
+//  Created by Paddy Reynolds on 6/07/2016.
+//  Copyright © 2016 Paddy Reynolds. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
